99e39b7425222358074258b0db70bf8091848298
PingPianoPong <wangka.senpai.6@gmail.com>
2025-06-10 20:21:00 +0100

    fix: reverted 'small UI change' commit which caused bugs in photo_modal.build()

--- lib/widgets/photo_modal.dart
+++ lib/widgets/photo_modal.dart
@@ -44,98 +44,96 @@ class PhotoModalState extends State<PhotoModal> {
     return AlertDialog(
       insetPadding: const EdgeInsets.all(8),
       contentPadding: const EdgeInsets.all(8),
-      content: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.85,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // image
-              ClipRRect(
-                borderRadius: BorderRadius.circular(24.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: FutureBuilder(
-                    future: BackEndService.fetchImageFromCloudByID(widget.post.imageIDs![0]), 
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return Text("Loading image...");
-                      } else {
-                        return Image.memory(snapshot.data!, fit: BoxFit.fitWidth);
-                      }
+      content: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.85,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(24.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: FutureBuilder(
+                  future: BackEndService.fetchImageFromCloudByID(widget.post.imageIDs![0]), 
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Text("Loading image...");
+                    } else {
+                      return Image.memory(snapshot.data!, fit: BoxFit.fitWidth);
                     }
-                  )
-                ),
+                  }
+                )
               ),
-              // scrollable comment section
-              Expanded(
-                child: Scrollbar(
-                  thumbVisibility: true,
-                  child: StreamBuilder(
-                    stream: BackEndService.getAllCommentSnapshotsFromChat(widget.post.chatID!), 
-                    builder: (context, snapshot) {
-                      print("in builder");
-                      if (snapshot.hasData) {
-                        print("snapshot has data");
-                        comments.clear();
-                        for (final doc in snapshot.data!.docs) {
-                          print("doc $doc");
-                          comments.add(CommentWidget(comment: Comment.fromFirestore(doc, null)));
-                          print("comments length = ${comments.length}");
+            ),
+            // scrollable comment section
+            Expanded(
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: StreamBuilder(
+                  stream: BackEndService.getAllCommentSnapshotsFromChat(widget.post.chatID!), 
+                  builder: (context, snapshot) {
+                    print("in builder");
+                    if (snapshot.hasData) {
+                      print("snapshot has data");
+                      comments.clear();
+                      for (final doc in snapshot.data!.docs) {
+                        print("doc $doc");
+                        comments.add(CommentWidget(comment: Comment.fromFirestore(doc, null)));
+                        print("comments length = ${comments.length}");
+                      }
+
+                      comments.sort((a, b) => (a.comment.postTime!.compareTo(b.comment.postTime!)));
+
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: comments.length,
+                        itemBuilder: (context, index) {
+                          return comments[index];
                         }
+                      );
 
-                        comments.sort((a, b) => (a.comment.postTime!.compareTo(b.comment.postTime!)));
-
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: comments.length,
-                          itemBuilder: (context, index) {
-                            return comments[index];
-                          }
-                        );
-
-                      } else {
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: 1,
-                          itemBuilder: (_, _) {
-                            return Text("Loading comments...");
-                          }
-                        );
-                      }
+                    } else {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 1,
+                        itemBuilder: (_, _) {
+                          return Text("Loading comments...");
+                        }
+                      );
                     }
-                  )
+                  }
+                )
+              ),
+            ),
+            // add comment input
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.emoji_emotions),
+                  onPressed: _addReaction,
                 ),
-              ),
-              // add comment input
-              Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.emoji_emotions),
-                    onPressed: _addReaction,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      controller: _controller,
-                      decoration: InputDecoration(
-                        labelText: 'Add a comment',
-                        border: OutlineInputBorder(),
-                        contentPadding: const EdgeInsets.all(8),
-                      ),
-                      onSubmitted: (_) {
-                        _addComment();
-                      }
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      labelText: 'Add a comment',
+                      border: OutlineInputBorder(),
+                      contentPadding: const EdgeInsets.all(8),
                     ),
+                    onSubmitted: (_) {
+                      _addComment();
+                    }
                   ),
-                  IconButton(
-                    icon: Icon(Icons.send),
-                    onPressed: _addComment,
-                  ),
-                ],
-              ),
-            ]
-          ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _addComment,
+                ),
+              ],
+            ),
+          ]
         ),
       ),
     );
