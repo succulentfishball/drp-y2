fc08d512bf7a2f766d1cdc7ddd96b1aa21258904
Kee Meng <49086005+KeeMeng@users.noreply.github.com>
2025-06-12 16:32:15 +0100

    Fixed comment section scrolling

--- lib/widgets/comment_modal.dart
+++ lib/widgets/comment_modal.dart
@@ -40,88 +40,94 @@ class CommentModalState extends State<CommentModal> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsetsGeometry.symmetric(vertical: 12),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // drag bar
-          Container(
-            width: 64,
-            height: 4,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(8),
+    return SizedBox(
+      height: 550,
+      child: Padding(
+        padding: EdgeInsetsGeometry.symmetric(vertical: 12),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // drag bar
+            Container(
+              width: 64,
+              height: 4,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
-          ),
-          // scrollable comment section
-          Scrollbar(
-            thumbVisibility: true,
-            child: StreamBuilder(
-              stream: BackEndService.getAllCommentSnapshotsFromChat(widget.post.chatID!), 
-              builder: (context, snapshot) {
-                print("in builder");
-                if (snapshot.hasData) {
-                  print("snapshot has data");
-                  comments.clear();
-                  for (final doc in snapshot.data!.docs) {
-                    print("doc $doc");
-                    comments.add(CommentWidget(comment: Comment.fromFirestore(doc, null)));
-                    print("comments length = ${comments.length}");
+            // scrollable comment section
+            SizedBox(
+              height: 470,
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: StreamBuilder(
+                  stream: BackEndService.getAllCommentSnapshotsFromChat(widget.post.chatID!), 
+                  builder: (context, snapshot) {
+                    print("in builder");
+                    if (snapshot.hasData) {
+                      print("snapshot has data");
+                      comments.clear();
+                      for (final doc in snapshot.data!.docs) {
+                        print("doc $doc");
+                        comments.add(CommentWidget(comment: Comment.fromFirestore(doc, null)));
+                        print("comments length = ${comments.length}");
+                      }
+
+                      comments.sort((a, b) => (a.comment.postTime!.compareTo(b.comment.postTime!)));
+
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: comments.length,
+                        itemBuilder: (context, index) {
+                          return comments[index];
+                        }
+                      );
+                    } else {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 1,
+                        itemBuilder: (_, _) {
+                          return Text("Loading comments...");
+                        }
+                      );
+                    }
                   }
-
-                  comments.sort((a, b) => (a.comment.postTime!.compareTo(b.comment.postTime!)));
-
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: comments.length,
-                    itemBuilder: (context, index) {
-                      return comments[index];
-                    }
-                  );
-                } else {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: 1,
-                    itemBuilder: (_, _) {
-                      return Text("Loading comments...");
-                    }
-                  );
-                }
-              }
-            )
-          ),
-          Spacer(),
-          // add comment input
-          Padding(
-            padding: EdgeInsetsGeometry.symmetric(horizontal: 8),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.emoji_emotions),
-                  onPressed: _addReaction,
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      labelText: 'Add a comment',
-                      border: OutlineInputBorder(),
-                      contentPadding: const EdgeInsets.all(8),
-                    ),
-                    onSubmitted: (_) {
-                      _addComment();
-                    }
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: _addComment,
-                ),
-              ],
+                )
+              ),
             ),
-          ),
-        ]
+            Spacer(),
+            // add comment input
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 8),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.emoji_emotions),
+                    onPressed: _addReaction,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        labelText: 'Add a comment',
+                        border: OutlineInputBorder(),
+                        contentPadding: const EdgeInsets.all(8),
+                      ),
+                      onSubmitted: (_) {
+                        _addComment();
+                      }
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: _addComment,
+                  ),
+                ],
+              ),
+            ),
+          ]
+        ),
       ),
     );
   }
